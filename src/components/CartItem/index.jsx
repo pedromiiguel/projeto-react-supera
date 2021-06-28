1f4b7dc6667016526d6e91a4d8a25a6e5cd58663
Pedro Miguel <pedromrap@gmail.com>
2021-06-28 16:46:50 -0300

    responsive cart

--- src/components/CartItem/index.jsx
+++ src/components/CartItem/index.jsx
@@ -5,28 +5,29 @@ import { convertCurrency } from '../../utils/convertCurrency';
 
 import DeleteIcon from '@material-ui/icons/Delete';
 
-import './styles.scss'
+import './styles.scss';
 export function CartItem({ product, index }) {
   const { handleRemoveProductToCart } = useContext(CartContext);
 
   return (
-    <div className="cart-item" >
-      <div className=" cart-item-header">
-        <img src={product.image} alt="" />
-        <p>{product.name}</p>
-      </div>
-
-      <p>1</p>
-      <p>{convertCurrency(product.price)}</p>
-      <button
-        className="delete-button"
-        title="Remover produto"
-        onClick={() => {
-          handleRemoveProductToCart(product, index);
-        }}
-      >
-        <DeleteIcon className="delete-icon" />
-      </button>
-    </div>
+    <tr>
+      <th className="cart-item-header">
+        <img src={product.image} alt={product.name} />
+        {product.name}
+      </th>
+      <th>1</th>
+      <th>{convertCurrency(product.price)}</th>
+      <th>
+        <button
+          className="delete-button"
+          title="Remover produto"
+          onClick={() => {
+            handleRemoveProductToCart(product, index);
+          }}
+        >
+          <DeleteIcon className="delete-icon" />
+        </button>
+      </th>
+    </tr>
   );
 }
--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -3,7 +3,7 @@ import { CartContext } from '../../context/CartContext';
 
 import { Link } from 'react-router-dom';
 import { Menu } from '../../components/Menu';
-import { CartItem } from '../../components/CartItem'
+import { CartItem } from '../../components/CartItem';
 
 import { convertCurrency } from '../../utils/convertCurrency';
 
@@ -11,10 +11,9 @@ import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 import './styles.scss';
 
-
 export function Cart() {
   const { cart, amount, freight } = useContext(CartContext);
-  
+
   return (
     <div id="page-cart">
       <header>
@@ -22,25 +21,34 @@ export function Cart() {
       </header>
       <main>
         <h1>Carrinho de compras</h1>
-        <div className="cart-items">
-          <div className="cart-title">
-            <p>Produto</p>
-            <p>Quantidade</p>
-            <p>Preço</p>
-            <p>Remover</p>
-          </div>
-          {cart.map((product, index) => (
-            <CartItem product={product} index={index} key={product.id}/>
-          ))}
+        <table id="data-table">
+          <thead>
+            <tr>
+              <th>Produto</th>
+              <th>Quantidade</th>
+              <th>Preço</th>
+              <th>Remover</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cart.map((product, index) => (
+              <CartItem product={product} index={index} key={product.id} />
+            ))}
+          </tbody>
+        </table>
 
-          <div className="checkout-container">
-            <Link to="/"> <span><ArrowBackIcon/></span> Continuar comprando</Link>
-            <div className="checkout">
-              <p>Frete: {convertCurrency(freight)}</p>
-              <p>Subtotal: {convertCurrency(amount)}</p>
-              <p>Total: {convertCurrency(freight + amount)}</p>
-              <button>Finalizar compra</button>
-            </div>
+        <div className="checkout-container">
+          <Link to="/">
+            <span>
+              <ArrowBackIcon />
+            </span>
+            Continuar comprando
+          </Link>
+          <div className="checkout">
+            <p>Frete: {convertCurrency(freight)}</p>
+            <p>Subtotal: {convertCurrency(amount)}</p>
+            <p>Total: {convertCurrency(freight + amount)}</p>
+            <button>Finalizar compra</button>
           </div>
         </div>
       </main>
